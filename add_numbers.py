ba276c463bec608447f956950acdb25051e3e769
aks-bit <aksembfw@gmail.com>
2023-03-18 22:04:51 +0530

    commit-1

---
+++ add_numbers.py
@@ -0,0 +1,10 @@
+# add three numbers
+
+import time
+num1 = input("First Number = ")
+num2 = input("\n Second Number = ")
+num3 = input("\n Third Number = ")
+
+sum = float(num1) + float(num2) + float(num3)
+
+print("sum of {0} and {1} and {2} is {3}".format(num1,num2,num3,sum))
